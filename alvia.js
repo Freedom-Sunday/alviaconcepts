4a7811ff3723635fbae40cc7ea2e4e9b9417e3ed
Freedom-Sunday <ifypariz16@gmail.com>
2025-05-29 14:55:27 +0100

    updated jquery
    added scroll animation on nav links

--- alvia.js
+++ alvia.js
@@ -19,8 +19,33 @@ $(document).ready(function () {
     $("#expand").show();
   });
 
-  $('.hero-texts').hide();
-  $('.hero-texts').fadeIn(5000);
+  // create a click function to scroll to the top of the page
+  $("#homebtn").click(function (e) {
+    e.preventDefault();
+    $("html, body").animate({ scrollTop: 0 }, "slow");
+  });
+
+  // create a click function to scroll to a particular element on the page
+  $("#aboutbtn").click(function (e) {
+    e.preventDefault();
+    var target = $(this).attr("href");
+    $("html, body").animate({ scrollTop: $('#about-container').offset().top }, "slow");
+  });
+
+  $("#partnerbtn").click(function (e) {
+    e.preventDefault();
+    var target = $(this).attr("href");
+    $("html, body").animate({ scrollTop: $('#partners').offset().top }, "slow");
+  });
+
+  $("#addressbtn").click(function (e) {
+    e.preventDefault();
+    var target = $(this).attr("href");
+    $("html, body").animate({ scrollTop: $('#address-con').offset().top }, "slow");
+  });
+
+  $(".hero-texts").hide();
+  $(".hero-texts").fadeIn(5000);
 
   
 });
